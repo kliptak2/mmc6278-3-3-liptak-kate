f10914231a651495ff2cf4ba6361e22a5327698e
Kate Liptak <kate.liptak@gmail.com>
2022-05-31 21:40:19 -0400

    Instructions & Starter Files

---
+++ index.js
@@ -0,0 +1,16 @@
+// Your code here
+var questionsArr = [
+    {
+      question: "A rhino's horn is made of hair",
+      answer: true
+    }
+]
+
+function runQuiz {}
+
+/* To Do
+1 - "before each" hook for "Should have start quiz button"
+2 - Define runQuiz
+3 - Use math.floor to round up score
+4 - Use "for" statment to loop over the array
+*/
